225593c370169c6644246411582ec1865a66113f
Mateusz <mateusz@matlinski.pl>
2019-05-22 19:19:26 +0200

    ip and location should work now

--- js/lang.js
+++ js/lang.js
@@ -23,48 +23,56 @@ var aboutUsBody = document.querySelectorAll(".about-us__body");
 var infoGallery = document.querySelector("#info");
 var footerText = document.querySelector(".main-footer__body");
 var gps;
-var access_key = '4d6e65113ea6aabbfe03da3daa5859f0';
+var myIp;
 window.onload = start;
 
 function start(){
-    $.ajax({
-        url: '//api.ipstack.com/check?access_key=' + access_key,
-        dataType: 'json',
-        success: function (json) {
+    fetch('https://api6.ipify.org?format=json')
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(response){
+            myIp = response.ip;
+            console.log(myIp);
 
-                // output the "capital" object inside "location"
-                $("span").text(json.location);
-                gps = json.country_name;
-        }
-    }).then(function(){
-        console.log(gps);
-        if(gps === "Poland"){
-            for(var i = 0; i < indexMenu.length; i++){
-                indexMenu[i].innerHTML = "STRONA GŁÓWNA";
-                gpsMenu[i].innerHTML = "LOKALIZACJA";
-                resMenu[i].innerHTML = "REZERWACJA";
-                contMenu[i].innerHTML = "KONTAKT";
-            }
-            aboutUsTitle.innerHTML = "Najlepsze apar&shy;tamenty w mieście!";
-            for(var i = 0; i < aboutUsBody.length; i++){
-                aboutUsBody[i].innerHTML = 'Na&shy;sze komfortowe apartamenty znajdują się w niez&shy;wyk&shy;łej, zie&shy;lo&shy;nej okolicy Bałut. W pobliżu są: ma&shy;low&shy;ni&shy;czy park i zielone skwery. Miejsce to jest bardzo dob&shy;rze skomu&shy;nikowane z centrum Łodzi. Blisko znaj&shy;duje się Wo&shy;je&shy;wódz&shy;ki Specjalistyczny Szpital im. dr. Wł. Bie&shy;gań&shy;skie&shy;go i Instytut Medycyny Pracy imienia prof. dra med. Jerzego Nofera. Bardzo blisko są skle&shy;py spo&shy;żyw&shy;cze, i pizzeria PRESTO. Zapraszamy!'
-            }
-            infoGallery.innerHTML = "Możesz zobaczyć galerię zdjęć z naszego apartamentu klikając na jeden z logotypów powyżej.";
-            footerText.innerHTML = "Wszelkie prawa zastrzeżone © 2019 autor witryny Mateusz Matliński.";
-        }
-        else if(gps === "Spain"){
-            for(var i = 0; i < indexMenu.length; i++){
-                indexMenu[i].innerHTML = "HOME";
-                gpsMenu[i].innerHTML = "LOCATION";
-                resMenu[i].innerHTML = "RESERVATION";
-                contMenu[i].innerHTML = "CONTACT";
-            }
-            aboutUsTitle.innerHTML = "The best apart&shy;ments in the city!";
-            for(var i = 0; i < aboutUsBody.length; i++){
-                aboutUsBody[i].innerHTML = 'Our com&shy;for&shy;ta&shy;ble apart&shy;ments are lo&shy;ca&shy;ted in the unique, green surround&shy;ings of Bałuty. Near&shy;by are: a pic&shy;tur&shy;esque park and green squares. This place is very well commu&shy;nica&shy;ted with the cen&shy;ter of Lodz. It is close to the Pro&shy;vincial Spe&shy;cia&shy;list Hos&shy;pital. dr. On. Biegański and the Insti&shy;tute of Occupa&shy;tional Medi&shy;cine, name of prof. Dr. med. Jerzy Nofer. The gro&shy;cery stores and the pizze&shy;ria PRESTO are ve&shy;ry close. Welcome!'
-            }
-            infoGallery.innerHTML = "You can see a photo gallery from any of our apartments by clicking on one of the logos above.";
-            footerText.innerHTML = "All rights reserved. © 2019 website author Mateusz Matliński.";
-        }
+            $.ajax({
+                url: 'https://ipapi.co/'+myIp+'/json/',
+                success: function (json) {
+        
+                        // output the "capital" object inside "location"
+                        $("span").text(json.location);
+                        gps = json.country_name;
+                }
+            }).then(function(){
+                console.log(gps);
+                if(gps === "Poland"){
+                    for(var i = 0; i < indexMenu.length; i++){
+                        indexMenu[i].innerHTML = "STRONA GŁÓWNA";
+                        gpsMenu[i].innerHTML = "LOKALIZACJA";
+                        resMenu[i].innerHTML = "REZERWACJA";
+                        contMenu[i].innerHTML = "KONTAKT";
+                    }
+                    aboutUsTitle.innerHTML = "Najlepsze apar&shy;tamenty w mieście!";
+                    for(var i = 0; i < aboutUsBody.length; i++){
+                        aboutUsBody[i].innerHTML = 'Na&shy;sze komfortowe apartamenty znajdują się w niez&shy;wyk&shy;łej, zie&shy;lo&shy;nej okolicy Bałut. W pobliżu są: ma&shy;low&shy;ni&shy;czy park i zielone skwery. Miejsce to jest bardzo dob&shy;rze skomu&shy;nikowane z centrum Łodzi. Blisko znaj&shy;duje się Wo&shy;je&shy;wódz&shy;ki Specjalistyczny Szpital im. dr. Wł. Bie&shy;gań&shy;skie&shy;go i Instytut Medycyny Pracy imienia prof. dra med. Jerzego Nofera. Bardzo blisko są skle&shy;py spo&shy;żyw&shy;cze, i pizzeria PRESTO. Zapraszamy!'
+                    }
+                    infoGallery.innerHTML = "Możesz zobaczyć galerię zdjęć z naszego apartamentu klikając na jeden z logotypów powyżej.";
+                    footerText.innerHTML = "Wszelkie prawa zastrzeżone © 2019 autor witryny Mateusz Matliński.";
+                }
+                else if(gps === "Spain"){
+                    for(var i = 0; i < indexMenu.length; i++){
+                        indexMenu[i].innerHTML = "HOME";
+                        gpsMenu[i].innerHTML = "LOCATION";
+                        resMenu[i].innerHTML = "RESERVATION";
+                        contMenu[i].innerHTML = "CONTACT";
+                    }
+                    aboutUsTitle.innerHTML = "The best apart&shy;ments in the city!";
+                    for(var i = 0; i < aboutUsBody.length; i++){
+                        aboutUsBody[i].innerHTML = 'Our com&shy;for&shy;ta&shy;ble apart&shy;ments are lo&shy;ca&shy;ted in the unique, green surround&shy;ings of Bałuty. Near&shy;by are: a pic&shy;tur&shy;esque park and green squares. This place is very well commu&shy;nica&shy;ted with the cen&shy;ter of Lodz. It is close to the Pro&shy;vincial Spe&shy;cia&shy;list Hos&shy;pital. dr. On. Biegański and the Insti&shy;tute of Occupa&shy;tional Medi&shy;cine, name of prof. Dr. med. Jerzy Nofer. The gro&shy;cery stores and the pizze&shy;ria PRESTO are ve&shy;ry close. Welcome!'
+                    }
+                    infoGallery.innerHTML = "You can see a photo gallery from any of our apartments by clicking on one of the logos above.";
+                    footerText.innerHTML = "All rights reserved. © 2019 website author Mateusz Matliński.";
+                }
+            })
     })
 }
