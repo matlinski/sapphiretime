27a9d549edf19a69605383c3a157c466f1771eb2
Mateusz <mateusz@matlinski.pl>
2019-05-24 19:00:30 +0200

    Trying to organise main.js

--- js/main.js
+++ js/main.js
@@ -1,80 +1,72 @@
 'use strict'
-document.addEventListener('DOMContentLoaded', function () {
-    
-    
-    
-    
-    
-    
-    
-    
-    singleLogo.addEventListener('click', revealS);
-    doubleLogo.addEventListener('click', revealD);
-    start(singleLogo, singleFrames, singlePhotos, doubleLogo, doubleFrames, doublePhotos);
-})
-start(document.querySelector('#single-logo'), document.querySelector('.single'), singleGallery.querySelectorAll('a'), singleGallery.querySelectorAll('img'), document.querySelector('#double-logo'), document.querySelector('.double'), doubleGallery.querySelectorAll('a'), doubleGallery.querySelectorAll('img')){
+
+function start(singleLogo, singleGallery, singleFrames, singlePhotos, doubleLogo, doubleGallery, doubleFrames, doublePhotos){
     var prevent = function(e) {
         e.preventDefault()
     }
     function reset(){
         for(var i = 0; i < singleFrames.length; i++){
-            singleFrames[i].addEventListener("click", prevent);
-            singleFrames[i].style.cursor = "not-allowed";
-        }
-    }
-    function resetRemove(){
-        for(var i = 0; i < singleFrames.length; i++){
-            singlePhotos[i].style.cursor = "pointer";
-            doublePhotos[i].style.cursor = "pointer";
-            singleFrames[i].style.cursor = "pointer";
-            singleFrames[i].setAttribute("data-lightbox", "mygallery");
-            singleFrames[i].removeEventListener("click", prevent);
+            singleFrames[i].addEventListener('click', prevent);
+            singleFrames[i].style.cursor = 'not-allowed';
         }
     }
     reset()
+    function resetRemove(){
+        for(var i = 0; i < singleFrames.length; i++){
+            singlePhotos[i].style.cursor = 'pointer';
+            doublePhotos[i].style.cursor = 'pointer';
+            singleFrames[i].style.cursor = 'pointer';
+            singleFrames[i].setAttribute('data-lightbox', 'mygallery');
+            singleFrames[i].removeEventListener('click', prevent);
+        }
+    }
     var revealS = function() {
         resetRemove()
-        doubleLogo.style.backgroundColor = "rgba(22%, 28%, 42%, 0.7)";
-        singleLogo.style.backgroundColor = "rgba(2%, 8%, 22%, .75)";
+        doubleLogo.style.backgroundColor = 'rgba(22%, 28%, 42%, 0.7)';
+        singleLogo.style.backgroundColor = 'rgba(2%, 8%, 22%, .75)';
         for(var i = 0; i < singlePhotos.length; i++){
             if (doubleGallery.style.display !== 'none') {
                 doubleGallery.style.display = 'none';
                 singleGallery.style.display = 'block';
             }
             
-            (singlePhotos[i].style.transform !== "scale(0.9)") ? singlePhotos[i].style.transform = "scale(0.9)" : console.log(singlePhotos[i]);
-            (singlePhotos[i].style.filter !== "blur(10px)") ? singlePhotos[i].style.filter = "blur(10px)" : console.log(singlePhotos[i]);
-            (singleFrames[i].style.opacity !== "0.4") ? singleFrames[i].style.opacity = "0.4" : console.log(singleFrames[i]);
+            (singlePhotos[i].style.transform !== 'scale(0.9)') ? singlePhotos[i].style.transform = 'scale(0.9)' : console.log(singlePhotos[i]);
+            (singlePhotos[i].style.filter !== 'blur(10px)') ? singlePhotos[i].style.filter = 'blur(10px)' : console.log(singlePhotos[i]);
+            (singleFrames[i].style.opacity !== '0.4') ? singleFrames[i].style.opacity = '0.4' : console.log(singleFrames[i]);
         }
         setTimeout(function(){
         for(var i = 0; i < singlePhotos.length; i++){
-            singlePhotos[i].style.transform = "scale(1)";
-            singlePhotos[i].style.filter = "blur(0)";
-            singleFrames[i].style.opacity = "0.85";
+            singlePhotos[i].style.transform = 'scale(1)';
+            singlePhotos[i].style.filter = 'blur(0)';
+            singleFrames[i].style.opacity = '0.85';
         }
         }, 250)
     }
-
-        var revealD = function() {
-            resetRemove()
-            singleLogo.style.backgroundColor = "rgba(22%, 28%, 42%, 0.7)";
-            doubleLogo.style.backgroundColor = "rgba(2%, 8%, 22%, .75)";
-            for(var i = 0; i < doublePhotos.length; i++){
-                if (singleGallery.style.display !== 'none') {
-                    singleGallery.style.display = 'none';
-                    doubleGallery.style.display = 'block';
-                }
-                (doublePhotos[i].style.transform !== "scale(0.9)") ? doublePhotos[i].style.transform = "scale(0.9)" : console.log(doublePhotos[i]);
-                (doublePhotos[i].style.filter !== "blur(10px)") ? doublePhotos[i].style.filter = "blur(10px)" : console.log(doublePhotos[i]);
-                (doubleFrames[i].style.opacity !== "0.4") ? doubleFrames[i].style.opacity = "0.4" : console.log(doubleFrames[i]);
-        }
-            setTimeout(function(){
-            for(var i = 0; i < doublePhotos.length; i++){
-                doublePhotos[i].style.transform = "scale(1)";
-                doublePhotos[i].style.filter = "blur(0)";
-                doubleFrames[i].style.opacity = "0.85";
+    var revealD = function() {
+        resetRemove()
+        singleLogo.style.backgroundColor = 'rgba(22%, 28%, 42%, 0.7)';
+        doubleLogo.style.backgroundColor = 'rgba(2%, 8%, 22%, .75)';
+        for(var i = 0; i < doublePhotos.length; i++){
+            if (singleGallery.style.display !== 'none') {
+                singleGallery.style.display = 'none';
+                doubleGallery.style.display = 'block';
             }
-            }, 250)
-        } 
+            (doublePhotos[i].style.transform !== 'scale(0.9)') ? doublePhotos[i].style.transform = 'scale(0.9)' : console.log(doublePhotos[i]);
+            (doublePhotos[i].style.filter !== 'blur(10px)') ? doublePhotos[i].style.filter = 'blur(10px)' : console.log(doublePhotos[i]);
+            (doubleFrames[i].style.opacity !== '0.4') ? doubleFrames[i].style.opacity = '0.4' : console.log(doubleFrames[i]);
+    }
+        setTimeout(function(){
+        for(var i = 0; i < doublePhotos.length; i++){
+            doublePhotos[i].style.transform = 'scale(1)';
+            doublePhotos[i].style.filter = 'blur(0)';
+            doubleFrames[i].style.opacity = '0.85';
+        }
+        }, 250)
+        singleLogo.addEventListener('click', revealS);
+        doubleLogo.addEventListener('click', revealD);
+    }
 }
-   
+
+document.addEventListener('DOMContentLoaded', function () {
+    start(document.querySelector('#single-logo'), document.querySelector('.single'), document.querySelectorAll('.single>a'), document.querySelectorAll('.single>a>img'), document.querySelector('#double-logo'), document.querySelector('.double'), document.querySelectorAll('.double>a'), document.querySelectorAll('.double>a>img'));
+})
--- js/serverCall.js
+++ js/serverCall.js
@@ -1,20 +1,20 @@
 
-"use strict"
+'use strict'
 
 document.addEventListener('DOMContentLoaded', function () {
     start();
 })
 
 function start() {
-    var flags = document.querySelectorAll(".flag");
+    var flags = document.querySelectorAll('.flag');
     var PL = flags[0];
     var UK = flags[1];
-    PL.addEventListener("click", chooseLang);
-    UK.addEventListener("click", chooseLang);
+    PL.addEventListener('click', chooseLang);
+    UK.addEventListener('click', chooseLang);
 
-    if (typeof localStorage.getItem("gps") === 'string' || localStorage.getItem("gps") instanceof String) {
-        if (localStorage.getItem("gps") !== "PL") localStorage.setItem('gps', "UK");
-        fetch("../json/" + localStorage.getItem("gps") + ".json").then(function (response) {
+    if (typeof localStorage.getItem('gps') === 'string' || localStorage.getItem('gps') instanceof String) {
+        if (localStorage.getItem('gps') !== 'PL') localStorage.setItem('gps', 'UK');
+        fetch('../json/' + localStorage.getItem('gps') + '.json').then(function (response) {
             return response.json();
         })
             .then(function (set) {
@@ -35,8 +35,8 @@ function start() {
             })
             .then(function (country) {
                 localStorage.setItem('gps', country);
-                if (country !== "PL") country = "UK";
-                fetch("../json/" + country + ".json")
+                if (country !== 'PL') country = 'UK';
+                fetch('../json/' + country + '.json')
                     .then(function (response) {
                         console.log(response);
                         return response.json();
@@ -52,23 +52,23 @@ function start() {
 }
 
 function langChange(init) {
-    var flags = document.querySelectorAll(".flag");
-    var indexMenu = document.querySelectorAll(".menu1");
-    var gpsMenu = document.querySelectorAll(".menu2");
-    var resMenu = document.querySelectorAll(".menu3");
-    var contMenu = document.querySelectorAll(".menu4");
-    var aboutUsTitle = document.querySelector(".about-us__title");
-    var aboutUsBody = document.querySelector(".about-us__body");
-    var infoGallery = document.querySelector("#info");
-    var footerText = document.querySelector(".main-footer__body");
+    var flags = document.querySelectorAll('.flag');
+    var indexMenu = document.querySelectorAll('.menu1');
+    var gpsMenu = document.querySelectorAll('.menu2');
+    var resMenu = document.querySelectorAll('.menu3');
+    var contMenu = document.querySelectorAll('.menu4');
+    var aboutUsTitle = document.querySelector('.about-us__title');
+    var aboutUsBody = document.querySelector('.about-us__body');
+    var infoGallery = document.querySelector('#info');
+    var footerText = document.querySelector('.main-footer__body');
     var singleGallery = document.querySelector('.single');
     var doubleGallery = document.querySelector('.double');
     var singlePhotos = singleGallery.querySelectorAll('img');
     var doublePhotos = doubleGallery.querySelectorAll('img');
-    var mainLogo = document.querySelector(".main-header__frame__logo");
-    var serviceLogo = document.querySelector(".single-alt");
-    var serviceLogoDouble = document.querySelector(".double-alt");
-    (localStorage.getItem("gps") === "PL") ? flags[0].classList.add('current') : flags[1].classList.add('current');
+    var mainLogo = document.querySelector('.main-header__frame__logo');
+    var serviceLogo = document.querySelector('.single-alt');
+    var serviceLogoDouble = document.querySelector('.double-alt');
+    (localStorage.getItem('gps') === 'PL') ? flags[0].classList.add('current') : flags[1].classList.add('current');
     document.title = init.title;
     mainLogo.alt = init.mainLogo;
     serviceLogo.alt = init.serviceLogo;
@@ -92,13 +92,13 @@ function langChange(init) {
 }
 
 function chooseLang(e) {
-    var flags = document.querySelectorAll(".flag");
+    var flags = document.querySelectorAll('.flag');
     localStorage.clear();
     localStorage.setItem('gps', e.target.alt);
     flags.forEach(function (i) {
         i.classList.remove('current');
     })
-    fetch("../json/" + localStorage.getItem("gps") + ".json").then(function (response) {
+    fetch('../json/' + localStorage.getItem('gps') + '.json').then(function (response) {
         return response.json();
     })
         .then(function (set) {
