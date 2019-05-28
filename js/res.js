65ad9763a4f77fce71da30c4b0a6f74a7383d495
Mateusz <mateusz@matlinski.pl>
2019-05-28 21:10:19 +0200

    comments work

--- js/res.js
+++ js/res.js
@@ -1,5 +1,12 @@
 var lang = 'pl';
 var comments = [];
+var stayedArea = document.querySelector(".reviews__stayed")
+var reviewedArea = document.querySelector(".reviews__reviewed")
+var picArea = document.querySelector(".reviews__pic")
+var nameArea = document.querySelector(".reviews__name>span")
+var countryArea = document.querySelector(".reviews__country")
+var positiveArea = document.querySelector(".reviews__positive")
+var scoreArea = document.querySelector(".reviews__score>span")
 fetch('json/single.' + lang + '.json')
   .then(res => res.json())
   .then(data => {
@@ -14,5 +21,23 @@ fetch('json/single.' + lang + '.json')
         score: data[i].score
       })
     }
-    console.log(comments)
+    qouteSwap(Math.floor(Math.random()*(comments.length-1)))
+    function qouteSwap(randomIndex){
+      console.log(randomIndex)
+      stayedArea.innerHTML = comments[randomIndex].stayed;
+        reviewedArea.innerHTML = comments[randomIndex].reviewed;
+        picArea.src = comments[randomIndex].pic;
+        nameArea.innerHTML = comments[randomIndex].name;
+        countryArea.innerHTML = comments[randomIndex].country;
+        positiveArea.innerHTML = comments[randomIndex].positive;
+        (comments[randomIndex].score !== null) ?scoreArea.innerHTML = comments[randomIndex].score+'/10':scoreArea.innerHTML = "N/A";
+    }
+    var randomIndex = 0;
+    var randomLength = 5000;
+    setInterval(function(){
+      randomIndex = Math.floor(Math.random()*(comments.length-1));
+      randomLength = comments[randomIndex].positive.length*650;
+      qouteSwap(randomIndex)  
+    },randomLength)
+    console.log(randomLength);
   })
