39930aa935acb5534e3bf8057dea4e674bf94ca0
matlinski <mateusz.matlinski@gmail.com>
2019-05-28 12:21:03 +0200

    quotes working more or less

--- js/res.js
+++ js/res.js
@@ -1,7 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    resStart();
+var arr = [];
+fetch('json/comment-single.htm')
+  .then(function(res){ return res.text()})
+  .then(function(data){
+    const parser = new DOMParser();
+    const doc = parser
+      .parseFromString(data, 'text/html');
+    return doc;
+}).then(function(doc){
+    arr = doc.querySelectorAll('.c-review__body');
+    console.log(arr)  
+    document.querySelector('.about-us__quotes').innerHTML = arr[(Math.floor(Math.random()*9)+1)].innerHTML
 })
-function resStart(){
-    var a = "lol";
-    console.log(a);
-}
