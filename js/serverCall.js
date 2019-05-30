35d8f0665039a7e246022c20b8e39eea63493d69
matlinski <mateusz.matlinski@gmail.com>
2019-05-30 22:19:17 +0200

    small change

--- js/serverCall.js
+++ js/serverCall.js
@@ -38,10 +38,7 @@ function serverStart(indexMenu,
                     ) {
 	document.querySelector('.our-links__frame.single').innerHTML += '<ins class="bookingaff" data-aid="1792900" data-target_aid="1792900" data-prod="rw" data-width="0" data-height="0" data-show_rw_logo="1" data-show_rw_badge="1" data-show_rw_text="1" data-show_rw_border="1" data-hid="1649307" data-lang="'+((localStorage.getItem('gps') === "PL") ?"pl" :"en")+'"><!-- Anything inside will go away once widget is loaded. --><a href="//www.booking.com?aid=1792900">Booking.com</a></ins>';
     document.querySelector('.our-links__frame.double').innerHTML += '<ins class="bookingaff" data-aid="1792901" data-target_aid="1792901" data-prod="rw" data-width="0" data-height="0" data-show_rw_logo="1" data-show_rw_badge="1" data-show_rw_text="1" data-show_rw_border="1" data-hid="2310850" data-lang="'+((localStorage.getItem('gps') === "PL") ?"pl" :"en")+'"> <!-- Anything inside will go away once widget is loaded. --> <a href="//www.booking.com?aid=1792901">Booking.com</a> </ins>';
-    var aparts = document.querySelectorAll('.our-links>*');
-    aparts.forEach(function(i){
-        i.style.transform = 'scaleY(1)';
-    })
+    
     function isInPage(node) {
         return (node === document.body) ? false : document.body.contains(node);
       }
