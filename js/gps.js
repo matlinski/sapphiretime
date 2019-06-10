e5c291afccdfc5f68eb2618ee74b3ddc8040af7a
matlinski <mateusz.matlinski@gmail.com>
2019-06-10 14:46:47 +0200

    map needs corection

--- js/gps.js
+++ js/gps.js
@@ -1,19 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     gpsStart(document.querySelector(".gps__map-container__cover"),
-            document.querySelectorAll(".gps__map-container__map"),
             document.querySelector("#info"),
             document.querySelector('#single-logo'),
-            document.querySelector('#double-logo')
+            document.querySelector('#double-logo'),
+            document.querySelector('.gps__map-container')
             );
     })
 
-function gpsStart(cover, maps, infoMap, singleLogo, doubleLogo){
-    var mapSingle = maps[0]
-    var mapDouble = maps[1]
+function gpsStart(cover, infoMap, singleLogo, doubleLogo, coverDiv){
 
-    var revealS = function() {
+    var mapSLoaded = false;
+    var mapDLoaded = false;
+
+    var revealS = function(e) {
+        e.preventDefault()
+        if(!mapSLoaded){
+            coverDiv.innerHTML += '<iframe class="gps__map-container__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7079.621650760226!2d19.431609981749283!3d51.79794370786818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bca903e63f943%3A0xc41c3844423b33ca!2sApartament+Sapphire+Time!5e1!3m2!1spl!2ses!4v1558770961811!5m2!1spl!2ses" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+            mapSLoaded = true;
+        }
+        var maps = document.querySelectorAll(".gps__map-container__map");
+        var mapSingle = maps[0]
         infoMap.style.display = "none";
-        cover.style.opacity = '0.4';
+        cover.style.opacity = '0';
         cover.style.display = "initial";
         setTimeout(function(){
             cover.style.opacity = '1';
@@ -30,7 +38,14 @@ function gpsStart(cover, maps, infoMap, singleLogo, doubleLogo){
             }, 500);
     }
 
-    var revealD = function() {
+    var revealD = function(e) {
+        e.preventDefault()
+        if(!mapDLoaded){
+            coverDiv.innerHTML += '<iframe class="gps__map-container__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7092.039225652966!2d19.426581112006417!3d51.796632065268525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bca96d54a2901%3A0x739b0adb38a124c!2sDouble+Sapphire+Time!5e1!3m2!1spl!2ses!4v1558770911998!5m2!1spl!2ses" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+            mapDLoaded = true;
+        }
+        var maps = document.querySelectorAll(".gps__map-container__map");
+        var mapDouble = maps[1]
         infoMap.style.display = "none";
         cover.style.opacity = '0.4';
         cover.style.display = "initial";
@@ -43,7 +58,7 @@ function gpsStart(cover, maps, infoMap, singleLogo, doubleLogo){
             i.style.display = "none";
         })
         setTimeout(function(){
-            mapDouble.style.display = "initial";
+            mapDouble.style.display = "block";
             cover.style.display = "none";
             mapDouble.style.opacity = "1";
             }, 500);
