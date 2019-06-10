4ac038ff0b4a36724801c685d1983d246c164057
Mateusz <mateusz@matlinski.pl>
2019-06-10 16:33:39 +0200

    map fixed

--- js/gps.js
+++ js/gps.js
@@ -14,15 +14,14 @@ function gpsStart(cover, infoMap, singleLogo, doubleLogo, coverDiv){
 
     var revealS = function(e) {
         e.preventDefault()
+        infoMap.style.display = "none";
         if(!mapSLoaded){
-            coverDiv.innerHTML += '<iframe class="gps__map-container__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7079.621650760226!2d19.431609981749283!3d51.79794370786818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bca903e63f943%3A0xc41c3844423b33ca!2sApartament+Sapphire+Time!5e1!3m2!1spl!2ses!4v1558770961811!5m2!1spl!2ses" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+            coverDiv.innerHTML += '<iframe style="z-index: 99" class="gps__map-container__map map-single" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7079.621650760226!2d19.431609981749283!3d51.79794370786818!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bca903e63f943%3A0xc41c3844423b33ca!2sApartament+Sapphire+Time!5e1!3m2!1spl!2ses!4v1558770961811!5m2!1spl!2ses" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
             mapSLoaded = true;
         }
         var maps = document.querySelectorAll(".gps__map-container__map");
-        var mapSingle = maps[0]
-        infoMap.style.display = "none";
-        cover.style.opacity = '0';
-        cover.style.display = "initial";
+        var mapSingle = document.querySelector(".map-single")
+        mapSingle.style.display = "none"
         setTimeout(function(){
             cover.style.opacity = '1';
             }, 200);
@@ -40,15 +39,16 @@ function gpsStart(cover, infoMap, singleLogo, doubleLogo, coverDiv){
 
     var revealD = function(e) {
         e.preventDefault()
+        infoMap.style.display = "none";
         if(!mapDLoaded){
-            coverDiv.innerHTML += '<iframe class="gps__map-container__map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7092.039225652966!2d19.426581112006417!3d51.796632065268525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bca96d54a2901%3A0x739b0adb38a124c!2sDouble+Sapphire+Time!5e1!3m2!1spl!2ses!4v1558770911998!5m2!1spl!2ses" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+            coverDiv.innerHTML += '<iframe style="z-index: 99" class="gps__map-container__map map-double" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7092.039225652966!2d19.426581112006417!3d51.796632065268525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bca96d54a2901%3A0x739b0adb38a124c!2sDouble+Sapphire+Time!5e1!3m2!1spl!2ses!4v1558770911998!5m2!1spl!2ses" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
             mapDLoaded = true;
         }
         var maps = document.querySelectorAll(".gps__map-container__map");
-        var mapDouble = maps[1]
+        console.log(maps)
+        var mapDouble = document.querySelector(".map-double")
+        mapDouble.style.display = "none"
         infoMap.style.display = "none";
-        cover.style.opacity = '0.4';
-        cover.style.display = "initial";
         setTimeout(function(){
             cover.style.opacity = '1';
             }, 200);
